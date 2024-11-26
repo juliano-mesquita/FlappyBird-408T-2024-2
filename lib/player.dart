9c7f26317ee31b54664ef22dadd4949236e53188
Kremis <291463@mesquita.com.br>
2024-11-26 14:38:08 -0300

    playerCompleto

--- lib/player.dart
+++ lib/player.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 
-class Player extends Component{
+class Player extends SpriteComponent{
   double posicao;
   double velocidade;
   bool estadoInicial;
@@ -11,4 +14,22 @@ class Player extends Component{
     this.velocidade = 0,
     this.estadoInicial = true,
   });
+
+  @override
+  FutureOr<void> onLoad() async {
+    
+    final image = await Flame.images.load('spritesPersonagem.png');
+    final size = Vector2(948, 865);
+
+    final playerFrame = Sprite(
+      image,
+      srcSize: size,
+    );
+
+    const playerSize = 128.0;
+    
+    sprite = playerFrame;
+    this.size = Vector2(playerSize, playerSize*(size.y/size.x));
+    return super.onLoad();
+  }
 }
