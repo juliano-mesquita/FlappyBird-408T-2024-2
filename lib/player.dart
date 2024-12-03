4098e9e4875ecab72575f93f26b14f8f431216a0
Henrique Kremis <291463@mesquita.com.br>
2024-12-03 16:22:50 -0300

    Implementação do Player (#30)
    * first commit
    
    * playerCompleto
    
    * Alteração nos sprites
    
    ---------
    
    Co-authored-by: juliano-mesquita <juliano@mesquita.com.br>

---
+++ lib/player.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flutter/material.dart';
+
+class Player extends SpriteComponent{
+  double posicao;
+  double velocidade;
+  bool estadoInicial;
+
+  Player({
+    required this.posicao,
+    this.velocidade = 0,
+    this.estadoInicial = true,
+  });
+
+  @override
+  FutureOr<void> onLoad() async {
+    
+    final image = await Flame.images.load('morcego1.png');
+    final size = Vector2(948, 865);
+
+    final playerFrame = Sprite(
+      image,
+      srcSize: size,
+    );
+
+    const playerSize = 128.0;
+    
+    sprite = playerFrame;
+    this.size = Vector2(playerSize, playerSize*(size.y/size.x));
+    return super.onLoad();
+  }
+}
