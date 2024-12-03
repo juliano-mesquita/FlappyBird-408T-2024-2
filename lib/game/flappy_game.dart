f13911d91983354c884945ea4f13392b1c839936
Bezin3488 <bernardopristupa@luteranodapaz.com.br>
2024-12-03 13:42:55 -0300

    integração dos assets (#39)
    * adicionei os assets ao pubspec
    
    * assets com imagens
    
    * assets com os morcegos
    
    * separação dos arquivos de jogo
    
    * efeitos e sons
    
    * Atualizado com o chão
    
    * estaca ok
    
    * adicionando o fundo

---
+++ lib/game/flappy_game.dart
@@ -0,0 +1,25 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class FlappyGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    debugPrint('Jogo carregado');
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    debugPrint('Update: $dt');
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    Paint cor = Paint();
+    cor.color = Colors.white;
+
+    canvas.drawCircle(Offset(size.x / 2, size.y / 2), 100, cor);
+  }
+}
