cd9e1fb4686c374f68e4cf218801cd7b9e08cf28
João Vitor <136394408+agsjohn@users.noreply.github.com>
2024-12-03 15:31:19 -0300

    Configuração Inicial do Firebase ao Flutter (#29)
    * first commit
    
    * segundo commit
    
    * third commit
    
    * commit
    
    * add pipeline

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flame/game.dart';
 import 'package:flappy_bat/game/flappy_game.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'game/flappy_game.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     GameWidget(game: FlappyGame()),
   );
