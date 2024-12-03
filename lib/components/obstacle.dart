9667fab220326f2c65dbd8a3dcd4a5feae94951c
Arruda <lucasbertholdp@gmail.com>
2024-12-03 17:18:22 -0300

    algumacoisaaisla

--- lib/components/obstacle.dart
+++ lib/components/obstacle.dart
@@ -1,22 +1,61 @@
+import 'dart:async';
 import 'dart:math';
-
 import 'package:flame/components.dart';
-import 'package:flame/events.dart';
+import 'package:flame/flame.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
-// Estrutura para representar as propriedades de um obstáculo
-class ObstacleData {
-  final double widthInPixels = 45;
-  double heightInPixels;
-  Vector2 position;
-  double intervalInSeconds = 3;
-  ObstacleData({
-    required this.heightInPixels,
-    required this.position,
-  });
+class ObstacleData extends SpriteComponent {
+  ObstacleData({required super.position});
 }
 
-class Obstacle extends SpriteComponent {
-  late ObstacleData obstacleData;
+class GameComponent extends PositionComponent {
+  double get bottomY => middlePoint + offset;
+  double get topY => middlePoint - offset;
+  int middlePoint = 0;
 
-  double meupau = (Random().nextDouble() * (0.9 - 0.1)) + 0.1;
+  double offset = 100;
+  late final max = (size.y * 0.8).round();
+  late final min = (size.y * 0.2).round();
+  late ObstacleData top;
+  late ObstacleData bottom;
+
+  bool isInitialized = false;
+
+  GameComponent({super.size});
+
+  @override
+  FutureOr<void> onLoad() async {
+    final topImage = await Flame.images.load('Estaca.png');
+    final bottomImage = await Flame.images.load('Estaca.png');
+
+    top = ObstacleData(position: Vector2(size.x, topY));
+    top.sprite = Sprite(topImage);
+    top.angle = 180 * (pi / 180);
+    bottom = ObstacleData(position: Vector2(size.x, bottomY));
+    bottom.sprite = Sprite(bottomImage);
+
+    add(top);
+    add(bottom);
+    isInitialized = true;
+  }
+
+  FutureOr<void> move(double position, double endOfScreen) {
+    if (isInitialized) {
+      top.position.x -= position;
+      bottom.position.x -= position;
+      debugPrint('positionX: ${top.position.x}');
+      debugPrint('positionY: ${top.position.y}');
+      debugPrint('positionY: ${bottom.position.y}');
+
+      if (top.position.x <= 0) {
+        middlePoint = Random().nextInt(max - min) + min;
+        top.position.x = endOfScreen + 87;
+        bottom.position.x = endOfScreen;
+
+        top.position.y = topY;
+        bottom.position.y = bottomY;
+      }
+    }
+  }
 }
