153d40c894d84c9f3e184e1e67e2633b7cda2bb4
thurim188 <292516@mesquita.com.br>
2024-12-17 15:24:12 -0300

    Update flappy_game.dart

--- lib/game/flappy_game.dart
+++ lib/game/flappy_game.dart
@@ -8,6 +8,23 @@ class FlappyGame extends FlameGame {
     super.onLoad();
     debugPrint('Jogo carregado');
   }
+  enum GameState { preStart, running, paused, gameOver }
+
+class StateManager {
+GameState _currentState = GameState.preStart;
+
+GameState get currentState => _currentState;
+
+void changeState(GameState newState) {
+_currentState = newState;
+print("Mudança de estado: $_currentState");
+}
+
+bool isRunning() => _currentState == GameState.running;
+bool isPaused() => _currentState == GameState.paused;
+bool isGameOver() => _currentState == GameState.gameOver;
+bool isPreStart() => _currentState == GameState.preStart;
+}
   
   @override
   void update(double dt) {
