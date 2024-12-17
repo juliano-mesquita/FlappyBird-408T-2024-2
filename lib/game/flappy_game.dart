6fa41e991694430e22546ab20ad78bffce769804
thurim188 <292516@mesquita.com.br>
2024-12-17 16:21:20 -0300

    Update flappy_game.dart

--- lib/game/flappy_game.dart
+++ lib/game/flappy_game.dart
@@ -8,68 +8,80 @@ class FlappyGame extends FlameGame {
     super.onLoad();
     debugPrint('Jogo carregado');
   }
+
   enum GameState { preStart, running, paused, gameOver }
 
-    class StateManager {
+  class StateManager {
     GameState _currentState = GameState.preStart;
 
     GameState get currentState => _currentState;
 
-      void changeState(GameState newState) {
+    void changeState(GameState newState) {
       _currentState = newState;
       print("Mudança de estado: $_currentState");
-      }
+    }
 
-      bool isRunning() => _currentState == GameState.running;
-      bool isPaused() => _currentState == GameState.paused;
-      bool isGameOver() => _currentState == GameState.gameOver;
-      bool isPreStart() => _currentState == GameState.preStart;
-      }
-  
+    bool isRunning() => _currentState == GameState.running;
+    bool isPaused() => _currentState == GameState.paused;
+    bool isGameOver() => _currentState == GameState.gameOver;
+    bool isPreStart() => _currentState == GameState.preStart;
+  }
+
+  // Funções que foram movidas para fora do switch
+  void showMenu() {
+    print("Exibindo menu inicial...");
+  }
+
+  void startGame(StateManager stateManager) {
+    print("Iniciando o jogo...");
+    stateManager.changeState(GameState.running);
+  }
+
+  void showScoreBoard() {
+    print("Exibindo placar de pontuação...");
+  }
+
+  void startGravity() {
+    print("Aplicando gravidade ao pássaro...");
+  }
+
+  void checkCollisions() {
+    print("Verificando colisões...");
+  }
+
+  void moveObstacles() {
+    print("Movendo os obstáculos...");
+  }
+
+  void resumeGame(StateManager stateManager) {
+    print("Retomando o jogo...");
+    stateManager.changeState(GameState.running);
+  }
+
+  void restartGame(StateManager stateManager) {
+    print("Reiniciando o jogo...");
+    stateManager.changeState(GameState.preStart);
+  }
+
   @override
   void update(double dt) {
     super.update(dt);
     switch (currentState) {
-        case GameState.preStart:
-         void showMenu() {
-print("Exibindo menu inicial...");
-}
-
-void startGame(StateManager stateManager) {
-print("Iniciando o jogo...");
-stateManager.changeState(GameState.running);
-}
-
-void showScoreBoard() {
-print("Exibindo placar de pontuação...");
-}
-          break;
-        case GameState.running:
-         void startGravity() {
-         print("Aplicando gravidade ao pássaro...");
-         }
-
-        void checkCollisions() {
-        print("Verificando colisões...");
-        }
-
-void moveObstacles() {
-print("Movendo os obstáculos...");
-}
-          break;
-        case GameState.paused:
-          void resumeGame(StateManager stateManager) {
-print("Retomando o jogo...");
-stateManager.changeState(GameState.running);
-}
-          break;
-        case GameState.gameOver:
-         void restartGame(StateManager stateManager) {
-         print("Reiniciando o jogo...");
-         stateManager.changeState(GameState.preStart);
-          break;
+      case GameState.preStart:
+        showMenu();
+        break;
+      case GameState.running:
+        startGravity();
+        checkCollisions();
+        moveObstacles();
+        break;
+      case GameState.paused:
+        resumeGame(StateManager());
+        break;
+      case GameState.gameOver:
+        restartGame(StateManager());
+        break;
     }
-
   }
 
   @override
@@ -81,4 +93,3 @@ stateManager.changeState(GameState.running);
     canvas.drawCircle(Offset(size.x / 2, size.y / 2), 100, cor);
   }
 }
-
