581e418e2f736ed5307fa2ee1993fd33adda6b75
thurim188 <292516@mesquita.com.br>
2024-12-17 15:10:56 -0300

    Update game_over.dart

--- lib/states/game_over.dart
+++ lib/states/game_over.dart
@@ -1,12 +1,6 @@
-import 'game_state.dart';
-
 class GameOverState {
-void showGameOverScreen() {
-print("Exibindo tela de fim de jogo...");
-}
 
 void restartGame(StateManager stateManager) {
 print("Reiniciando o jogo...");
 stateManager.changeState(GameState.preStart);
-}
 }
